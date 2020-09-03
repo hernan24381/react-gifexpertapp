eea92f2bd89ac2206cbde77e34b6cbaaac317950
Hernan Vlachovsky <hernan24381@gmail.com>
2020-09-02 22:21:21 -0300

    docs agregado

---
+++ docs/precache-manifest.33af05587a476a0752db7715d16edae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b666bcd3d9ffe39c5c7ff86d068db898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396cde547bd892f6efd6",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "396cde547bd892f6efd6",
+    "url": "/static/js/main.a3783e15.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
